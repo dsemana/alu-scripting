de6b31938da324629b4e1e5df03ffa0e522c80fb
semana-divin <semanadivin@gmail.com>
2026-03-25 14:55:12 +0200

    new

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,18 +4,20 @@ import requests
 
 
 def top_ten(subreddit):
+    """Queries Reddit API and prints titles of first 10 hot posts."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "mozilla/5.0"}
     params = {"limit": 10}
 
     try:
         response = requests.get(
-            url, headers=headers, params=params, allow_redirects=True
+            url, headers=headers, params=params, allow_redirects=False
         )
     except Exception:
         return
 
     if response.status_code != 200:
+        print(None)
         return
 
     data = response.json()
