048cff21fd8042e51af4f3bb1489d1490aedce39
semana-divin <semanadivin@gmail.com>
2026-03-25 14:52:25 +0200

    fixed

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,22 +2,27 @@
 """Prints the titles of the first 10 hot posts listed for a given subreddit."""
 import requests
 
-# Function to get the top 10 subreddits
+
 def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "linux:my.reddit.app:v1.0.0 (by /u/elie)"}
+    headers = {"User-Agent": "mozilla/5.0"}
     params = {"limit": 10}
 
-    response = requests.get(
-        url, headers=headers, params=params, allow_redirects=False
-    )
+    try:
+        response = requests.get(
+            url, headers=headers, params=params, allow_redirects=True  
+        )
+    except Exception:
+        return
 
     if response.status_code != 200:
-        print("Error occuring")
-        return
+        return  
 
     data = response.json()
     posts = data.get("data", {}).get("children", [])
+
+    if not posts:
+        return
 
     for post in posts:
         title = post.get("data", {}).get("title")
